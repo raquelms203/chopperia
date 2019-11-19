3d3bb37a951c77061b88071707697a891ed4109c
Raquel <raquelmartins203@yahoo.com.br>
2019-11-19 17:14:33 -0200

    Merge

--- src/view/Maquina.java
+++ src/view/Maquina.java
@@ -33,6 +33,18 @@ public class Maquina extends JFrame {
 	JComboBox<String> comboBoxRefrigerante;
 
 	public static void main(String[] args) {
+		try {
+			for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+				if ("Metal".equals(info.getName())) {
+					javax.swing.UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| javax.swing.UnsupportedLookAndFeelException ex) {
+			System.err.println(ex);
+		}
+
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
